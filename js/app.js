8b4bb5ca9c5954e10d183fe52fac3a2d7527dd7c
nespython <ernestfaytone@gmail.com>
2017-10-31 14:18:17 +0800

    updated appjs
    updated appjs

--- js/app.js
+++ js/app.js
@@ -332,27 +332,6 @@
                 radius: currRadius
             });
 
-
-            var drawingManager = new google.maps.drawing.DrawingManager({
-                drawingMode: google.maps.drawing.OverlayType.MARKER,
-                drawingControl: true,
-                drawingControlOptions: {
-                    position: google.maps.ControlPosition.TOP_CENTER,
-                    drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-                },
-                markerOptions: {
-                    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-                },
-                circleOptions: {
-                    fillColor: '#ffff00',
-                    fillOpacity: 1,
-                    strokeWeight: 5,
-                    clickable: false,
-                    editable: true,
-                    zIndex: 1
-                }
-            });
-            drawingManager.setMap(map);
             setSearchMap();
 
             directionsDisplay.setMap(map);
